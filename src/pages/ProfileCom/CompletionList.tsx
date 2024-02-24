946fa48fd44bdd26de9f0b439fe279c2d16e3c4b
john <johnzengshi@gmail.com>
2024-02-24 11:20:24 +0800

    Merge branch 'dev' of https://github.com/JohnZengshi/roos-dapp into dev

--- src/pages/ProfileCom/CompletionList.tsx
+++ src/pages/ProfileCom/CompletionList.tsx
@@ -1,24 +1,41 @@
 import './CompletionList.scss'
 import './CompletionList-m.scss'
+import { useEffect, useState } from 'react'
+import { API_RECOMMEND_LIST, API_RECOMMEND_LIST_ITEM, API_RECOMMEND_LIST_PARAMS } from '@/utils/api'
+
+const DEFAULTPAGINATION: API_RECOMMEND_LIST_PARAMS = {
+    page: 1,
+    size: 5
+}
 
 export const CompletionList = () => {
 
-    const data = [{
-        id: 1,
-        address: '123123',
-        odyssey: 'xxxx',
-        points: '10'
-    }, {
-        id: 2,
-        address: '123123',
-        odyssey: 'xxxx',
-        points: '10'
-    }, {
-        id: 3,
-        address: '123123',
-        odyssey: 'xxxx',
-        points: '10'
-    },]
+    const [pagination, setPagination] = useState<API_RECOMMEND_LIST_PARAMS>(DEFAULTPAGINATION)
+    const [list, setList] = useState<Array<API_RECOMMEND_LIST_ITEM>>([])
+
+    const handleQueryList = async (params: API_RECOMMEND_LIST_PARAMS) => {
+        const result = await API_RECOMMEND_LIST(params)
+        setList(result)
+    }
+
+    useEffect(() => {
+        handleQueryList(DEFAULTPAGINATION)
+    }, [])
+
+    const renderOdyssey = (type: 1 | 2 | 3 | 4 | 5) => {
+        switch (type) {
+            case 1:
+                return '链接钱包';
+            case 2:
+                return '关注推特';
+            case 3:
+                return '加入TG';
+            case 4:
+                return '加入Discord ';
+            case 5:
+                return '邀请获得积分';
+        }
+    }
 
     return <ul className='CompletionListBox'>
         <li className='header'>
@@ -31,14 +48,14 @@ export const CompletionList = () => {
             <div className='item'>Points</div>
         </li>
         {
-            data.map(item => <li>
-                <div className='item'>{item.id}</div>
+            list.map((item, index) => <li>
+                <div className='item'>{index + 1}</div>
                 <div className='line' />
                 <div className='item'>{item.address}</div>
                 <div className='line' />
-                <div className='item'>{item.odyssey}</div>
+                <div className='item'>{renderOdyssey(item.type)}</div>
                 <div className='line' />
-                <div className='item'>{item.points}</div>
+                <div className='item'>{item.number}</div>
             </li>)
         }
     </ul>
--- src/pages/ProfileCom/Recommend.tsx
+++ src/pages/ProfileCom/Recommend.tsx
@@ -1,26 +1,39 @@
 import "./Recommend.scss";
 import "./Recommend-m.scss";
+import { useEffect, useState } from "react";
+import { API_GET_MY_POINT, BOX_MY_POINT } from "@/utils/api";
 
 export const Recommend = () => {
 
+    const [state, setState] = useState<BOX_MY_POINT>()
+
+    const initData = async () => {
+        const result: BOX_MY_POINT = await API_GET_MY_POINT()
+        setState(result)
+    }
+
+    useEffect(() => {
+        initData()
+    }, [])
+
     return <div className="Recommend">
         <div className="item">
-            <span>10</span>
+            <span>{state?.iveFinished || '-'}</span>
             <span>I finished Odyssey</span>
         </div>
         <div className="line" />
         <div className="item">
-            <span>10</span>
+            <span>{state?.recommend || '-'}</span>
             <span>recommend Finish</span>
         </div>
         <div className="line" />
         <div className="item">
-            <span>10</span>
+            <span>{state?.myIntegral || '-'}</span>
             <span>My Oydssey points</span>
         </div>
         <div className="line" />
         <div className="item">
-            <span>10</span>
+            <span>{state?.recommendationPoints || '-'}</span>
             <span>Recommended to get</span>
         </div>
     </div>
