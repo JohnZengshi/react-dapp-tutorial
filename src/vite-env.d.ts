f4f69c5d0ecf54b7a2e83306eb3cbcb5638afe22
john <johnzengshi@gmail.com>
2024-03-12 18:08:07 +0800

    🐎 ci:
    修改环境变量数据类型

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_BASE_API_URL: string;
   readonly VITE_BASE_URL: string;
-  readonly VITE_NETWORK_USDT_ADDRESS: string;
+  readonly VITE_NETWORK_USDT_ADDRESS: `0x${string}`;
   readonly VITE_CHECK_TRANSACTION_DETAILS_URL: string;
   // 更多环境变量...
 
