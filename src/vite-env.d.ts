49efae4f7b3ad379a71727a5e1257835c211df61
john <johnzengshi@gmail.com>
2024-03-18 16:34:49 +0800

    🐎 ci:
    添加新的环境变量VITE_PARTICIPATE_CHAIN_ID

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /*
  * @LastEditors: John
  * @Date: 2024-03-08 09:44:08
- * @LastEditTime: 2024-03-15 17:34:20
+ * @LastEditTime: 2024-03-18 13:45:44
  * @Author: John
  */
 /// <reference types="vite/client" />
@@ -9,6 +9,7 @@
 interface ImportMetaEnv {
   readonly VITE_BASE_API_URL: string;
   readonly VITE_BASE_URL: string;
+  readonly VITE_PARTICIPATE_CHAIN_ID: number;
   readonly VITE_NETWORK_USDT_ADDRESS: `0x${string}`;
   readonly VITE_CONTRACT_ADDRESS: `0x${string}`;
   readonly VITE_CHECK_TRANSACTION_DETAILS_URL: string;
